4e0d5f60ff3931e8f03c364a135402e9d84e9a3e
sauhardjain <sauhardjain03@gmail.com>
2024-03-15 11:33:25 -0700

    styling: setup shadcn and updates

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,25 +1,34 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { cn } from "../lib/utils";
 
 const inter = Inter({
-  variable: "--font-inter", 
-  subsets: ["latin"] 
+  variable: "--font-inter",
+  subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
   title: "Hackathon Management Suite",
-  description: "Hackathon Management Suite is a suite of tools to help you manage your hackathon.",
+  description:
+    "Hackathon Management Suite is a suite of tools to help you manage your hackathon.",
 };
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          inter.variable
+        )}
+      >
+        {children}
+      </body>
     </html>
-  )
+  );
 }
