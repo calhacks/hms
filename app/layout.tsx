98fa8346f5edbd93f9d711cf2c2be92b61aed48b
Jinlin Zhang <kennyzhang0819@outlook.com>
2024-03-19 15:49:14 -0700

    Dashboard complete shadcn redesign

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,11 @@
+import Providers from "@/components/layout/providers";
+import { Toaster } from "@/components/ui/toaster";
+import "@uploadthing/react/styles.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { cn } from "../lib/utils";
+import { getServerSession } from "next-auth";
 
 const inter = Inter({
   variable: "--font-inter",
@@ -14,20 +18,19 @@ export const metadata: Metadata = {
     "Hackathon Management Suite is a suite of tools to help you manage your hackathon.",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const session = await getServerSession();
   return (
-    <html lang="en">
-      <body
-        className={cn(
-          "min-h-screen bg-background font-sans antialiased",
-          inter.variable
-        )}
-      >
-        {children}
+    <html lang="en" suppressHydrationWarning>
+      <body className={cn("min-h-screen bg-background font-sans antialiased", inter.variable)}>
+        <Providers session={session}>
+          <Toaster />
+          {children}
+        </Providers>
       </body>
     </html>
   );
