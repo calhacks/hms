414713b747f21ca903c5684121a9c0f09bc9080f
Jinlin Zhang <kennyzhang0819@outlook.com>
2024-04-01 14:41:22 -0700

    minor fix

--- app/dashboard/create-hackathon/page.tsx
+++ app/dashboard/create-hackathon/page.tsx
@@ -1,5 +1,7 @@
 import { CreateHackathonOne } from '../../../components/create-hackathon-form';
 
+
+
 export default function Page() {
     return (
         <div className="flex-1 space-y-4 p-4 md:p-8 pt-6">
